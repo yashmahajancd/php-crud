ee63e29641548b12a0c9587457b2580c6c1ae92e
Yash Mahajan <mahajanyash2185@gmail.com>
2026-07-03 23:46:20 +0530

    add navbar

--- includes/header.php
+++ includes/header.php
@@ -9,3 +9,56 @@
 </head>
 
 <body class="bg-dark text-light">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow border-bottom border-secondary">
+    <div class="container">
+
+        <a class="navbar-brand fw-bold d-flex align-items-center" href="index.php">
+            <span class="bg-success rounded-circle me-2"
+                style="width:10px; height:10px;"></span>
+            DataVault
+        </a>
+
+        <!-- Mobile Toggle -->
+        <button class="navbar-toggler" type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">
+                        Records
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">
+                        Add User
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Export
+                    </a>
+                </li>
+
+            </ul>
+
+            <div class="d-flex gap-5 align-items-center">
+
+                <button onclick="window.location.href=''" class="btn btn-outline-light btn-sm px-3">
+                    Admin Panel
+                </button>
+
+            </div>
+        </div>
+
+    </div>
+</nav>
